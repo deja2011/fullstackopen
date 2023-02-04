c2d8a6c0ab20b4dd4be695ebf4798955c57a82e3
Lawrence <lawrence@binance.com>
2023-02-04 23:58:17 +0800

    Complete exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+import axios from 'axios'
 
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
@@ -6,12 +8,7 @@ import Filter from './components/Filter'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [pattern, setPattern] = useState('')
@@ -49,6 +46,14 @@ const App = () => {
   const handlePatternChange = (event) => {
     setPattern(event.target.value.trim().toLowerCase())
   }
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   let filteredPersons = persons
   if (pattern) {
