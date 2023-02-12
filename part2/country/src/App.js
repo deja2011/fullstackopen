37dfd2c454e69ed3858e3126ab7a8fa828fec5f3
Lawrence <lawrence@binance.com>
2023-02-12 19:43:06 +0800

    Complete exercise 2.19

--- part2/country/src/App.js
+++ part2/country/src/App.js
@@ -4,20 +4,20 @@ import services from './services/countries'
 
 import './index.css'
 
-const Filter = ({pattern, handlePatternChange}) =>
-    <form>
-        <div>
-            <span>find countries</span><input value={pattern} onChange={handlePatternChange} />
-        </div>
-    </form>
+const Filter = ({ pattern, handlePatternChange }) =>
+  <form>
+    <div>
+      <span>find countries</span><input value={pattern} onChange={handlePatternChange} />
+    </div>
+  </form>
 
-const CountryList = ({fCountries}) => {
+const CountryList = ({ fCountries, handleShowCountryOf }) => {
   if (fCountries.length >= 10) {
     return <div>Too many matches, specify another filter</div>
   } else if (fCountries.length > 1) {
     return (
       <div>
-          {fCountries.map(c => <div key={c.name.common}>{c.name.common}</div>)}
+        {fCountries.map(c => <CountrySelector key={c.name.common} country={c} handleShowCountry={handleShowCountryOf(c)}/>)}
       </div>
     )
   } else {
@@ -25,33 +25,57 @@ const CountryList = ({fCountries}) => {
   }
 }
 
-const CountryDetail = ({fCountries}) => {
-  if (fCountries.length === 1) {
-    let c = fCountries[0]
+const CountrySelector = ({ country, handleShowCountry }) => {
+  return (
+    <div>
+      <span>{country.name.common}</span>
+      <button onClick={handleShowCountry}>show</button>
+    </div>
+  )
+}
+
+const CountryDetail = ({ country }) => {
+  if (country !== null) {
     return (
       <div>
-        <h1>{c.name.common}</h1>
-        <div>capital {c.capital[0]}</div>
-        <div>area {c.area}</div>
+        <h1>{country.name.common}</h1>
+        <div>capital {country.capital[0]}</div>
+        <div>area {country.area}</div>
         <h2>languages:</h2>
         <ul>
-          {Object.values(c.languages).map(l => <li key={l}>{l}</li>)}
+          {Object.values(country.languages).map(l => <li key={l}>{l}</li>)}
         </ul>
-        <img class="flag" src={c.flags.png} alt={c.name.common}></img>
+        <img className="flag" src={country.flags.png} alt={country.name.common}></img>
       </div>
     )
+  } else {
+    return <div></div>
   }
-
-return (<div></div>)
 }
 
 const App = () => {
 
   const [countries, setCountries] = useState([])
+  const [country, setCountry] = useState(null)
   const [pattern, setPattern] = useState('')
+
+  let filteredCountries = countries.filter(c => c.name.common.toLowerCase().includes(pattern))
+
+  console.log(
+    "rendered app number of countries:", countries.length,
+    "country is null:", country === null,
+    "pattern is:", pattern,
+    "numboer of filtered countries:", filteredCountries.length
+  )
 
   const handlePatternChange = (event) => {
     setPattern(event.target.value.trim().toLowerCase())
+    filteredCountries = countries.filter(c => c.name.common.toLowerCase().includes(pattern))
+    if (filteredCountries.length === 1) {
+      setCountry(filteredCountries[0])
+    } else {
+      setCountry(null)
+    }
   }
 
   useEffect(() => {
@@ -59,14 +83,20 @@ const App = () => {
     services.getAll().then(data => setCountries(data))
   }, [])
 
-  let filteredCountries = countries.filter(c => c.name.common.toLowerCase().includes(pattern))
+  const handleShowCountryOf = (country) => {
+    return () => {
+      setCountry(country)
+    }
+  }
 
   return (
-  <div>
+    <div>
       <Filter pattern={pattern} handlePatternChange={handlePatternChange} />
-      <CountryList fCountries={filteredCountries} />
-      <CountryDetail fCountries={filteredCountries} />
-  </div>
+      {country === null ?
+        <CountryList fCountries={filteredCountries} handleShowCountryOf={handleShowCountryOf} /> :
+        <CountryDetail country={country} />
+      }
+    </div>
   )
 }
 
